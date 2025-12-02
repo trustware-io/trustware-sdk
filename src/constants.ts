2b120c83521af75c3c3589b08529eb551bfa22a9
DadlessNsad <tbodnar85@gmail.com>
2025-12-02 11:14:45 -0700

    one line to rule them all

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // constants.ts
 export const SDK_NAME = "@trustware/sdk";
-export const SDK_VERSION = "1.0.3";
+export const SDK_VERSION = "1.0.4";
 //export const API_ROOT = "http://localhost:8000"; // local dev
 export const API_ROOT = "https://api.trustware.io";
 // Your Go server mounts at /api (no /v1 in routes), so the SDK pins the *host*
